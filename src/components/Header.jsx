41bcc86927cf8ff987edff5a1bc7c84852d588cf
Lokesh <lokesh.apr24@gmail.com>
2024-10-22 18:24:23 +0530

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ const Header = () => {
         <header className="d-flex align-items-center justify-content-between">
            <a href="/"><img src="/assets/images/react.png" alt="" /></a>
            <div className="">
+            Header
              
            </div>
            
